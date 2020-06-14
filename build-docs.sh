9346423d584ab0531d10fa2a397fb0994e92e945
thautwarm <twshere@outlook.com>
2020-06-14 20:37:40 +0900

    up

---
+++ build-docs.sh
@@ -0,0 +1,8 @@
+git checkout gh-pages
+git merge -X theirs master
+python manage.py --clean
+python manage.py --build
+git add -A
+git commit -m "docs update"
+git push origin gh-pages
+git checkout master
